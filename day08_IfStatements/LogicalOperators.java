46d4c3ea757b849c989d9c43ada27556d0cd3a12
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-03-07 22:00:56 +0300

    If Statements

--- day08_IfStatements/LogicalOperators.java
+++ day08_IfStatements/LogicalOperators.java
@@ -3,6 +3,17 @@ package day08_IfStatements;
 public class LogicalOperators {
 
     public static void main(String[] args) {
+        String name = "Steven";
+
+        int age =19;
+        String citizen = " USA";
+
+        boolean isEligible = age >=18 && citizen == "USA";
+        //                     19>= 18 && "UK" ==   "USA"
+        //                       true  && false ===> false
+
+        System.out.println(name + " is eligible to vote:" + isEligible);
+
 
 
     }
