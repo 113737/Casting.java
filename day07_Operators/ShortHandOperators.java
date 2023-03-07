fe1836b42cd9ec1c007ceb4736839fecef0bf6b3
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-03-07 07:31:33 +0300

    Java Programming Videos

---
+++ day07_Operators/ShortHandOperators.java
@@ -0,0 +1,29 @@
+package day07_Operators;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ShortHandOperators {
+    public static void main(String[] args) {
+
+        //assignment: =
+        int number = 100;
+
+        System.out.println("number = " + number);//100
+
+        number = 200;
+        System.out.println("number = " + number);
+        
+        String word= "Java Programming";
+
+        System.out.println("word = " + word);//Java Programming
+
+        word = "Wooden Spoon";
+        System.out.println("word = " + word);// Wooden Spoon
+        
+        word= "Cydeo";
+        System.out.println("word = " + word);
+        
+             
+
+    }
+}
