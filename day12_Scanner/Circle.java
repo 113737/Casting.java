66869f2e7fbbcd7c656611200fbfa4c989f918ac
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-03-15 18:44:57 +0300

    day11 part 2

---
+++ day12_Scanner/Circle.java
@@ -0,0 +1,28 @@
+package day12_Scanner;
+
+import java.util.Scanner;
+
+public class Circle {
+
+    public static void main(String[] args) {
+        Scanner radius = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle");
+
+        double r = radius.nextDouble();
+        double area= r*r*3.14;
+        double perimeter= 2*r*3.14;
+
+        System.out.println("area = " + area);
+        System.out.println("perimeter = " + perimeter);
+
+        radius.close();
+
+
+    }
+}
+/*
+1. Circle:
+
+        1.1 Ask the user to enter the radius of the circle
+        1.1 Calculate the area and perimeter of the circle by using the radius
+*/
