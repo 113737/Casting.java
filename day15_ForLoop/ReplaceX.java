adbe9f6f673f5cb069398102517de8bbb7df9b14
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-03-20 00:35:16 +0300

    java day14

---
+++ day15_ForLoop/ReplaceX.java
@@ -0,0 +1,10 @@
+package day15_ForLoop;
+
+public class ReplaceX {
+    public static void main(String[] args) {
+        String str="xcodeX";
+        System.out.println(str.replace("x", "a").replace("X", "a"));
+
+
+    }
+}
