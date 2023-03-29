4cc391028f784f4b1fb2e9e76dad204f0675d08c
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-03-29 08:07:50 +0300

    day20 Arrays practice2

---
+++ day20_Arrays/ArraysPractice2.java
@@ -0,0 +1,43 @@
+package day20_Arrays;
+
+import java.util.Arrays;
+
+public class ArraysPractice2 {
+
+    public static void main(String[] args) {
+
+        char[]letters= new char [26];
+       /* letters[0]='A';
+        letters[1]='B';
+        ...
+
+        */
+
+       /* for (char i= 'A',j=0; i<='Z'&& j<letters.length;i++,j++){
+            letters[j]=i;
+            //letters[i]=(char)j;
+        }
+
+        */
+        char ch= 'A';
+        for (int i = 0; i < letters.length; i++) {
+            letters[i]=ch++;
+
+        }
+
+        System.out.println(Arrays.toString(letters));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
