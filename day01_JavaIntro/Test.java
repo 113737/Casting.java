2c0fc3a5b50e9857c9f49abc50dd0169e2badd2e
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-04-06 13:57:29 +0300

    day24

---
+++ day01_JavaIntro/Test.java
@@ -0,0 +1,17 @@
+package day01_JavaIntro;
+
+import day24_CustomMethodReturn.ReturnMethodPractice4;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String str="aaaaaaaaaabbbbbbbbbbbbbbbb";
+        ReturnMethodPractice4.removeDuplicates(str);
+        System.out.println(str);
+    }
+
+
+
+
+
+}
