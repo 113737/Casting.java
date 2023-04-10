0e3989471b4ed5905b2bfb1d08ca1b3b89cf7238
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-04-10 16:05:53 +0300

    utility array

---
+++ Utilities/ArrayUtility.java
@@ -0,0 +1,36 @@
+package Utilities;
+
+public class ArrayUtility {
+
+    //print each integer of an integer array in separate lines
+    public static void printEach(int[]array){
+        for (int each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+    //print each double of a double array in separate lines
+
+    public static void printEach(double[]array){
+        for (double each : array) {
+            System.out.println(each);
+        }
+    }
+
+
+    //print each char of a char array in separate lines
+    public static void printEach(char[]array){
+        for (char each : array) {
+            System.out.println(each);
+        }
+    }
+
+    //print each String of a String array in separate lines
+    public static void printEach(String[]array){
+        for (String each : array) {
+            System.out.println(each);
+        }
+    }
+
+}
