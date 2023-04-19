1c71755b57ec8b0bda4668cf0c4bd4ce259a9d4c
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-04-19 05:42:04 +0300

    day28_ArrayList password

--- day28_ArrayList/StrongPassword.java
+++ day28_ArrayList/StrongPassword.java
@@ -31,7 +31,33 @@ public class StrongPassword {
 boolean isStrongPassword= r1 &&r2 && r3 && r4 && r5;
         System.out.println("isStrongPassword = " + isStrongPassword);
 
-}}
+}
+public static boolean isStrongPassword(String password){
+    boolean r1=password.length()>=8 && !password.contains(" ");
+    boolean r2=false;//has upper case
+    boolean r3=false;//has lower case
+    boolean r4=false;//has special char
+    boolean r5=false;// has digit
+    char[]chars= password.toCharArray();
+    for (char each : chars) {
+
+        if(Character.isUpperCase(each)){
+            r2 = true;
+        }else if(Character.isLowerCase(each)){
+            r3 = true;
+        }else if(Character.isDigit(each)){
+            r5 = true;
+        }else{ // special char
+            r4 = true;
+        }
+
+    }
+    boolean isStrongPassword = r1 && r2 && r3 && r4 && r5;
+    return isStrongPassword;
+
+}
+
+}
 /*
 1.Write a program that can verify if a password is a strong password. Characteristics of strong passwords are:
         1.1 Password must be at least have 8 characters long,and should not contain space
