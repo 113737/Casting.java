f7914309c429ff18a44fc69d895c2c6895cd97bf
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-04-20 23:34:18 +0300

    day 29

--- day29_ArrayList/NthLargestNumber.java
+++ day29_ArrayList/NthLargestNumber.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class NthlargestNumber {
+public class NthLargestNumber {
 
     public static void main(String[] args) {
         ArrayList<Integer>list= new ArrayList<>();
