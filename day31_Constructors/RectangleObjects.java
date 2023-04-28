af1ed08a5db781321d707309582cf1a4b4013350
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-04-28 22:31:54 +0300

    day 31

---
+++ day31_Constructors/RectangleObjects.java
@@ -0,0 +1,18 @@
+package day31_Constructors;
+
+public class RectangleObjects {
+    public static void main(String[] args) {
+
+    }
+
+}
+/*
+create a custom class named Rectangle
+    Attributes:
+        length, width
+    Add a constructor that can set all the fields
+    Actions:
+        calculateArea(): calculates the area of the rectangle, returns it as double
+        calculatePerimeter() calculates the perimeter of the rectangle, returns it as double
+     	toString(): displays the width, length, area and perimeter of the rectangle when an object of rectangle passed in the print statement
+ */
