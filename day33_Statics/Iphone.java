e34c3be3ef19a5a11a55eaabcaf20c41f6b89a29
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-04-29 14:18:42 +0300

    day 33

---
+++ day33_Statics/Iphone.java
@@ -0,0 +1,22 @@
+package day33_Statics;
+
+public class Iphone {
+
+    public static String brand="Apple";
+    public String model;
+    public double price;
+
+    public static String OS="iOS";
+    public String color,size;
+
+    public static String madeIn="China";
+    public static boolean hasBattery=true;
+    public static boolean isTouchScreen=true;
+    public static void printOperatingSystem(){
+        System.out.println(OS);
+    }
+public void method1(){
+    System.out.println(model+" : "+price);
+    System.out.println(OS);
+}
+}
